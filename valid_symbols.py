af2f37f7fc765ce8a13a365f700839450c9045f0
katmsreyes <katrinamsreyes@berkeley.edu>
2025-09-19 16:31:17 -0700

    Update valid_symbols.py

--- valid_symbols.py
+++ valid_symbols.py
@@ -1,4 +1,21 @@
+
 class Solution:
     def valid_symbols(self, s: str) -> bool:
         # TODO: Implement
-        pass
+        checking_pair = []
+        opening_chars = ["(","[","{"]
+        closing_chars = ([")","]","}"])
+        pair = dict(zip(")]}", "([{"))
+
+
+        for i in s:
+            if i in opening_chars:
+                checking_pair.append(i)
+            elif i in closing_chars:
+                if not checking_pair or s.pop() != pair[s]:
+                    return False
+                
+        return len(checking_pair) == 0
+    
+
+    #solution references the Week3_async.pdf slide on Stack: iterative solution
