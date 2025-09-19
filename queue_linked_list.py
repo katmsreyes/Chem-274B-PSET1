533202f0423fb41bdebf34fe5734c42c91bbea22
katmsreyes <katrinamsreyes@berkeley.edu>
2025-09-19 16:36:34 -0700

    Update queue_linked_list.py

--- queue_linked_list.py
+++ queue_linked_list.py
@@ -11,11 +11,33 @@ class Queue:
 
     def enqueue(self, val):
         # TODO: Implement
-        pass
+
+        new_node = ListNode(val)
+
+        self.rear.insert(new_node)
+
+        if self.size == 0 : #queue is empty
+            new_node = self.front = self.rear
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+
+
 
     def dequeue(self):
         # TODO: Implement
-        pass
+        
+        if self.size != 0:
+            self.front = self.front.next
+            self.size -=1
+            if self.size == 0:
+                self.rear = None
+                print('the queue is now empty')
+        
+        else:
+            print('queue is empty')
+            
+
 
     def peek(self):
         if self.is_empty():
@@ -24,9 +46,10 @@ class Queue:
 
     def is_empty(self):
         # TODO: Implement
-        pass
+        if self.size ==0:
+            print('queue is empty')
 
     def get_size(self):
         return self.size
     
-    
+ 
